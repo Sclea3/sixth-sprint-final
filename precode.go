7332e3c87aec0090a696bd7d401952d33033d5a0
sclea3-indrive <aleksandr.golubkin@indriver.com>
2024-11-17 18:12:32 +0500

    task done

--- precode.go
+++ precode.go
@@ -4,49 +4,17 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/Yandex-Practicum/go-rest-api-homework/handlers"
 	"github.com/go-chi/chi/v5"
 )
-
-// Task ...
-type Task struct {
-	ID           string   `json:"id"`
-	Description  string   `json:"description"`
-	Note         string   `json:"note"`
-	Applications []string `json:"applications"`
-}
-
-var tasks = map[string]Task{
-	"1": {
-		ID:          "1",
-		Description: "Сделать финальное задание темы REST API",
-		Note:        "Если сегодня сделаю, то завтра будет свободный день. Ура!",
-		Applications: []string{
-			"VS Code",
-			"Terminal",
-			"git",
-		},
-	},
-	"2": {
-		ID:          "2",
-		Description: "Протестировать финальное задание с помощью Postmen",
-		Note:        "Лучше это делать в процессе разработки, каждый раз, когда запускаешь сервер и проверяешь хендлер",
-		Applications: []string{
-			"VS Code",
-			"Terminal",
-			"git",
-			"Postman",
-		},
-	},
-}
-
-// Ниже напишите обработчики для каждого эндпоинта
-// ...
 
 func main() {
 	r := chi.NewRouter()
 
-	// здесь регистрируйте ваши обработчики
-	// ...
+	r.Get("/tasks", handlers.GetTasks)
+	r.Post("/tasks", handlers.PostTask)
+	r.Get("/tasks/{id}", handlers.GetTaskByID)
+	r.Delete("/tasks/{id}", handlers.DeleteTaskByID)
 
 	if err := http.ListenAndServe(":8080", r); err != nil {
 		fmt.Printf("Ошибка при запуске сервера: %s", err.Error())
